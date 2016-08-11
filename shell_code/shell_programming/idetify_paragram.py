cf7a1809f3ff0308aa793d460685ed36bcc8f21d
zhangxun <15025689012@163.com>
2016-08-11 20:11:26 +0800

    identify shell parameters

---
+++ shell_code/shell_programming/idetify_paragram.py
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+E_WRONG_AGRS=65
+script_parameters="-a -h -m -z"
+
+if [ $# -ne $Number_of_expected_args ]
+then
+    echo "Usage:`basename $0`  $script_parameters"
+    #basename $0   script  filename
+    exit $E_WRONG_ARGS
+fi
